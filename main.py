b2b718dacb198d76a6cdbca0814a21bda772c30c
Noam Cohen <Noamiko173@gmail.com>
2025-11-30 00:50:37 +0200

    updates

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ files: list[str] = glob(f"{Path(__file__).parent}/stuff/*/setup*")
 for i in files:
     run(f"python {i}", shell=True, check=True)
 run(f"clear && source {Path.home()}/.bashrc", shell=True, check=True)
-answer = input("launch linutil? [Y/n] \n")
-if answer != "n":
+answer = input("launch linutil? [y/N] \n")
+if answer != "y":
     run("curl -fsSL https://christitus.com/linux | sh", shell=True, check=True)
 answer = input("launch hyprland? [Y/n] \n")
 if answer != "n":
